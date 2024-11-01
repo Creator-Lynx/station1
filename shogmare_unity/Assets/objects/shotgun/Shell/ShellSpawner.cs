f02ad56b103619ddcde8a229c7bd9db8368a57d2
Creator-Lynx <1_linx_1@mail.ru>
2024-11-01 17:46:39 +0300

    added simple shell spawner

---
+++ shogmare_unity/Assets/objects/shotgun/Shell/ShellSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    [SerializeField] GameObject shell;
+    public void SpawnShell()
+    {
+        Instantiate(shell);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SpawnShell();
+        }
+    }
+}
