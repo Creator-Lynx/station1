8452e9e5de2075d389ba39036ea465a59e1ef380
Creator-Lynx <1_linx_1@mail.ru>
2024-11-02 19:47:22 +0300

    modified shell spawner, make it more clearly in scene

--- shogmare_unity/Assets/objects/shotgun/Shell/ShellSpawner.cs
+++ shogmare_unity/Assets/objects/shotgun/Shell/ShellSpawner.cs
@@ -7,12 +7,17 @@ public class ShellSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Shells = GameObject.Find("SHELLS"); //find
+        if (!Shells)
+        {
+            Debug.LogError("Need to create 'SHELLS' empty object to grouping dropped shells");
+        }
     }
     [SerializeField] GameObject shell;
+    GameObject Shells;
     public void SpawnShell()
     {
-        GameObject currentShell = Instantiate(shell);
+        GameObject currentShell = Instantiate(shell, Shells.transform);
 
     }
     // Update is called once per frame
