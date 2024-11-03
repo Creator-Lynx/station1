7c22de44541ab79b5de23c190387aaa4435ab715
Creator-Lynx <1_linx_1@mail.ru>
2024-11-03 17:18:39 +0300

    made correctly dropping shels - shell spawner: force, rotations connecting - shell phys material - in-scene corrected data and prefabed - import settings for fbx to set animations events

--- shogmare_unity/Assets/objects/shotgun/Shell/ShellSpawner.cs
+++ shogmare_unity/Assets/objects/shotgun/Shell/ShellSpawner.cs
@@ -15,21 +15,44 @@ public class ShellSpawner : MonoBehaviour
     }
     [SerializeField] GameObject shell, shellPos1, shellPos2;
     GameObject Shells;
+    [SerializeField] float ExtructionForce = 2f, ExtructionTorque = 2;
+    GameObject currentShell1, currentShell2;
+    Rigidbody rig1, rig2;
     public void SpawnShell()
     {
-        GameObject currentShell = Instantiate(shell,
+        currentShell1 = Instantiate(shell,
          shellPos1.transform.position, shellPos1.transform.rotation,
-         Shells.transform);
-        GameObject currentShell2 = Instantiate(shell,
+         shellPos1.transform);
+        currentShell2 = Instantiate(shell,
          shellPos2.transform.position, shellPos2.transform.rotation,
-         Shells.transform);
+         shellPos2.transform);
+        /*this block needs to convert scale of prebab and shotgun, when using new Shotgun fbx
+        you need to revert this two lines*/
+        currentShell1.transform.localScale = 0.01f * Vector3.one;
+        currentShell2.transform.localScale = 0.01f * Vector3.one;
+        //=======================================
+        rig1 = currentShell1.GetComponent<Rigidbody>();
+        rig2 = currentShell2.GetComponent<Rigidbody>();
+        rig1.useGravity = false;
+        rig2.useGravity = false;
+        mass = rig1.mass;
     }
-    // Update is called once per frame
-    void Update()
+    float mass;
+    public void UnconnectShell()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            SpawnShell();
-        }
+        rig1.AddRelativeForce(Vector3.left * ExtructionForce, ForceMode.Impulse);
+        rig2.AddRelativeForce(Vector3.left * ExtructionForce, ForceMode.Impulse);
+        currentShell1.transform.SetParent(Shells.transform);
+        currentShell2.transform.SetParent(Shells.transform);
+
+        rig1.useGravity = true;
+        rig2.useGravity = true;
+
+        rig1.AddForce(transform.forward * ExtructionForce * 0.35f, ForceMode.Impulse);
+        rig2.AddForce(transform.forward * ExtructionForce * 0.35f, ForceMode.Impulse);
+
+
+        rig1.AddTorque(Random.onUnitSphere * ExtructionTorque, ForceMode.Impulse);
+        rig2.AddTorque(Random.onUnitSphere * ExtructionTorque, ForceMode.Impulse);
     }
 }
