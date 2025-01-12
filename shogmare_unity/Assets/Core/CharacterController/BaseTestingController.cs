9204058d0c8b2069101326a633e19cdb5cfcb16f
Creator-Lynx <1_linx_1@mail.ru>
2025-01-12 17:51:58 +0300

    moving and x-looking controller (test ver)

--- shogmare_unity/Assets/Core/CharacterController/BaseTestingController.cs
+++ shogmare_unity/Assets/Core/CharacterController/BaseTestingController.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class BaseTestingController : MonoBehaviour
 {
@@ -13,9 +9,22 @@ public class BaseTestingController : MonoBehaviour
     [SerializeField] string secondShot = "secondShot";
     [SerializeField] string doubleShot = "doubleShot";
     [SerializeField] string reload = "reloadHalf";
-
+    [Space]
+    [Header("Moving Controller")]
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float MoveSpeed = 5f;
     bool isFirstShot = true;
+    [Header("Mouse Sensivity")]
+    [SerializeField]
+    float mouseSensivity = 5f;
+    const float mouseSensivityConst = 1f;
     void Update()
+    {
+        ShootingInput();
+        MovinInput();
+        CameraInput();
+    }
+    void ShootingInput()
     {
         //shooting input
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -46,7 +55,7 @@ public class BaseTestingController : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKey(KeyCode.R))
         {
             if (!isFirstShot)
             {
@@ -56,8 +65,48 @@ public class BaseTestingController : MonoBehaviour
         }
 
     }
+    void MovinInput()
+    {
+        Vector3 moving = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            moving.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moving.z = -1;
+        }
+        else moving.z = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            moving.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moving.x = 1;
+        }
+        else moving.x = 0;
+        moving.Normalize();
+        Vector3 finalMoving =
+        transform.forward * moving.z +
+        transform.right * moving.x;
+        characterController.Move(finalMoving * Time.deltaTime * MoveSpeed);
+    }
+    void CameraInput()
+    {
+        float x = Input.GetAxis("Mouse X") * mouseSensivity * mouseSensivityConst;
+
+        characterController.transform.Rotate(Vector3.up, x);
+    }
+    private void FixedUpdate()
+    {
+
+    }
     private void Start()
     {
         isFirstShot = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
     }
 }
