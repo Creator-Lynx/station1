b4326973c1b9b296636dd78b9366d695f0a25aad
Creator-Lynx <1_linx_1@mail.ru>
2025-01-12 08:36:03 +0300

    created testing input for shooting

--- shogmare_unity/Assets/Core/CharacterController/BaseTestingController.cs
+++ shogmare_unity/Assets/Core/CharacterController/BaseTestingController.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BaseTestingController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    Animator ShotgunAnimator;
+    [Header("Animator Triggers")]
+    [SerializeField] string firstShot = "firstShot";
+    [SerializeField] string secondShot = "secondShot";
+    [SerializeField] string doubleShot = "doubleShot";
+    [SerializeField] string reload = "reloadHalf";
 
-    // Update is called once per frame
+    bool isFirstShot = true;
     void Update()
     {
-        
+        //shooting input
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (isFirstShot)
+            {
+                ShotgunAnimator.SetTrigger(firstShot);
+                isFirstShot = false;
+            }
+            else
+            {
+                ShotgunAnimator.SetTrigger(secondShot);
+                isFirstShot = true;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (isFirstShot)
+            {
+                ShotgunAnimator.SetTrigger(doubleShot);
+            }
+            else
+            {
+                ShotgunAnimator.SetTrigger(secondShot);
+                isFirstShot = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isFirstShot)
+            {
+                isFirstShot = true;
+                ShotgunAnimator.SetTrigger(reload);
+            }
+        }
+
+    }
+    private void Start()
+    {
+        isFirstShot = true;
     }
 }
