05a1132127231e3124890b02c07e1ef49c31332d
CreatorLynx <1_linx_1@mail.ru>
2025-04-12 18:00:05 +0300

    working on realization of old shooting

--- shogmare_unity/Assets/Core/CharacterController/CharacterShootingGameplayController.cs
+++ shogmare_unity/Assets/Core/CharacterController/CharacterShootingGameplayController.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class CharacterShootingGameplayController : MonoBehaviour
 {
-    
+
+    [SerializeField] string firstShot = "firstShot";
+    [SerializeField] string secondShot = "secondShot";
+    [SerializeField] string doubleShot = "doubleShot";
+    [SerializeField] string reload = "reloadHalf";
+    bool isFirstShot = true;
+        
+    [SerializeField] Animator ShotgunAnimator;
+    public void Shoot()
+    {
+         if (isFirstShot)
+            {
+                ShotgunAnimator.SetTrigger(firstShot);
+                isFirstShot = false;
+            }
+            else
+            {
+                ShotgunAnimator.SetTrigger(secondShot);
+                isFirstShot = true;
+            }
+    }
 }
