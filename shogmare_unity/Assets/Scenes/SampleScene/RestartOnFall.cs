ea1fb20c40d1c53716d0170e45163b1983b286c2
CreatorLynx <1_linx_1@mail.ru>
2025-04-20 18:02:50 +0300

    make black screen & restart on fall

--- shogmare_unity/Assets/Scenes/SampleScene/RestartOnFall.cs
+++ shogmare_unity/Assets/Scenes/SampleScene/RestartOnFall.cs
@@ -1,10 +1,39 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RestartOnFall : MonoBehaviour
 {
+
+    [SerializeField]
+    Image BlackScreen;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) SceneManager.LoadScene(0);
+        if (other.CompareTag("Player")) 
+        {
+            StartCoroutine(FallDeathAnimation());
+            
+        }
+    }
+
+    IEnumerator FallDeathAnimation()
+    {   
+        Color c = Color.black;
+        c.a = 0;
+        for (int i = 0; i < 100; i++)
+        {
+            yield return new WaitForSeconds(0.005f);
+            c.a = i * 0.01f;
+            BlackScreen.color = c;
+        }
+        yield return new WaitForSeconds(3f);
+        ReloadLevel();
+    }
+
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 }
